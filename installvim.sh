7fcafd010aa29ad263dd8f2be805131717f6777d
Stefan Beck <stefan@itbeck.de>
2020-03-03 11:20:58 +0100

    initial commit

---
+++ installvim.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/vim-airline/vim-airline.git/ ~/.vim/bundle/vim-airline
+git clone https://github.com/kien/ctrlp.vim.git/ ~/.vim/bundle/ctrlp.vim
